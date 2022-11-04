e8d1fa0db32ff486f09041ad963ad2466b073045
Skylar <skylar.som@gmail.com>
2022-11-04 00:34:02 -0400

    Add surpression

--- src/main/java/xyz/brassgoggledcoders/iberiarediscovered/content/RediscoveredRecipes.java
+++ src/main/java/xyz/brassgoggledcoders/iberiarediscovered/content/RediscoveredRecipes.java
@@ -5,6 +5,7 @@ import net.minecraftforge.common.crafting.IIngredientSerializer;
 import xyz.brassgoggledcoders.iberiarediscovered.recipe.ingredient.PotionIngredient;
 import xyz.brassgoggledcoders.iberiarediscovered.recipe.ingredient.PotionIngredientSerializer;
 
+@SuppressWarnings("unused")
 public class RediscoveredRecipes {
     public static IIngredientSerializer<PotionIngredient> POTION_INGREDIENT = CraftingHelper.register(
             PotionIngredient.NAME,
